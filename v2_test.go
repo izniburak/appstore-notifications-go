ab5b657544cc1188adb17f95d5834c700e8b1f9c
Michael Dawson <michaeldawson@Michaels-MacBook-Pro.local>
2023-12-04 20:41:27 +1100

    add test for apple test notification request

--- v2_test.go
+++ v2_test.go
@@ -9,9 +9,9 @@ import (
 
 func TestServerNotificationV2(t *testing.T) {
 	// {"signedPayload":"..."}
-	appStoreServerRequest := os.Getenv("APPLE_NOTIFICATION_REQUEST")
+	appStoreServerRequest := os.Getenv("APPLE_NOTIFICATIONNOTIFICATION_REQUEST")
 	if appStoreServerRequest == "" {
-		panic("No valid AppStoreServerRequest")
+		t.Skip("No valid AppStoreServerRequest")
 	}
 	var request AppStoreServerRequest
 	err := json.Unmarshal([]byte(appStoreServerRequest), &request) // bind byte to header structure
@@ -22,7 +22,7 @@ func TestServerNotificationV2(t *testing.T) {
 	// -----BEGIN CERTIFICATE----- ......
 	rootCert := os.Getenv("APPLE_CERT")
 	if rootCert == "" {
-		panic("Apple Root Cert not available")
+		t.Skip("Apple Root Cert not available")
 	}
 
 	appStoreServerNotification := New(request.SignedPayload, rootCert)
@@ -38,4 +38,38 @@ func TestServerNotificationV2(t *testing.T) {
 	println(appStoreServerNotification.Payload.Data.BundleId)
 	println(appStoreServerNotification.TransactionInfo.ProductId)
 	fmt.Printf("Product Id: %s", appStoreServerNotification.RenewalInfo.ProductId)
+}
+
+func TestServerTestNotificationV2(t *testing.T) {
+	// App store test notification issued after requesting a test notification
+	// {"signedPayload":"..."}
+	appStoreServerRequest := os.Getenv("APPLE_NOTIFICATION_TEST_REQUEST")
+	if appStoreServerRequest == "" {
+		t.Skip("No valid AppStoreServerTestRequest")
+	}
+	var request AppStoreServerRequest
+	err := json.Unmarshal([]byte(appStoreServerRequest), &request)
+	if err != nil {
+		panic(err)
+	}
+
+	// -----BEGIN CERTIFICATE----- ......
+	rootCert := os.Getenv("APPLE_CERT")
+	if rootCert == "" {
+		t.Skip("Apple Root Cert not available")
+	}
+
+	appStoreServerNotification := New(request.SignedPayload, rootCert)
+
+	if !appStoreServerNotification.IsValid {
+		t.Error("Payload is not valid")
+	}
+
+	if appStoreServerNotification.Payload.Data.Environment != "Sandbox" {
+		t.Errorf("got %s, want Sandbox", appStoreServerNotification.Payload.Data.Environment)
+	}
+
+	println(appStoreServerNotification.Payload.Data.BundleId)
+	fmt.Printf("Notification Type: %s\n", appStoreServerNotification.Payload.NotificationType)
+	fmt.Printf("Notification Sub-Type: %s\n", appStoreServerNotification.Payload.Subtype)
 }
